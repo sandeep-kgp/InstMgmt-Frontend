c84e1d205bcf46702503b3ae9fbd5593c2894ed6
Sandeep  Shukla <sandeep.kgp@gmail.com>
2018-09-16 14:41:18 +0530

    Changes

--- webpack.config.js
+++ webpack.config.js
@@ -23,11 +23,23 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use:["babel-loader"]
+        use:['babel-loader']
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        include: [/node_modules/],
+        use: [
+          "style-loader",
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: [/node_modules/],
+        use: [
+          'style-loader', 
+          { loader: 'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]' },
+        ]
       }
     ]
   },
